2094b7bcec1e0a965583d03240d6d9818cd94b6a
Victhor Gabriel <105661120+Victh0rG@users.noreply.github.com>
2025-09-03 07:49:10 -0300

    feat: adcionados os principais módulos
    objetivo, rodar o projeto no ambiente de código principal

--- __main__.py
+++ __main__.py
@@ -1,3 +1,8 @@
+from src.data_collection import collect_news
+from src.data_cleaning import clean_data
+from src.sentiment_analysis import analyze_sentiments
 
-if __name__ == '__main__':
-    print("hello word")
+if __name__ == "__main__":
+    csv_raw = collect_news()
+    csv_clean = clean_data(csv_raw)
+    analyze_sentiments(csv_clean)
